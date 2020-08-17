fb72afa5fdbc188b8ee33ee0e10732e851aa687f
Muhammed Hasan Ince <mhasanince@gmail.com>
2020-08-17 23:01:55 +0300

    initial commit

---
+++ precache-manifest.3ef9f5952b850d1ff1f574087cffadfb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15fdd945d7cecc0505d83e4f39cbfece",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5cf5aff9ee966124b71",
+    "url": "/static/css/main.5a28aade.chunk.css"
+  },
+  {
+    "revision": "3128d524bd1e88ef920f",
+    "url": "/static/js/2.543afee9.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.543afee9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5cf5aff9ee966124b71",
+    "url": "/static/js/main.8e70bfc6.chunk.js"
+  },
+  {
+    "revision": "bb0ebfe53c6eb0f0751e",
+    "url": "/static/js/runtime-main.2e9eab52.js"
+  }
+]);
